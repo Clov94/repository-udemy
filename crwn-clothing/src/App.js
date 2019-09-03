2f2992ab7826a056f9c5ee5d2aea2d3dbfa83e05
Clov94 <loswaldoc94@gmail.com>
2019-09-02 23:51:37 -0500

    stylesOn-scss

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./home-page.component";
+import './'
 
 function App() {
   return (
-    <div>
+    <div >
       <HomePage />
     </div>
   );
--- crwn-clothing/src/home-page.component.jsx
+++ crwn-clothing/src/home-page.component.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "./home-page.styles.scss";
+
 const HomePage = () => (
   <div className="homePage">
     <div className="directory-menu">
